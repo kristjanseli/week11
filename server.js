79936d687e7007e3944b9afc380a3546bf59cb8d
Kristjan <kristjan@example.com>
2026-06-02 14:34:10 +0300

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("OK ✅");
